a2a55485df1a6b2f40a5093949efe8fc7975fbc9
manyyeon <udayeon@naver.com>
2022-09-06 19:52:05 +0900

    feat : 주식 통장 페이지 추가

---
+++ src/screens/passbook/StockPassbookScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const StockPassbookScreen = () => {
+  return (
+    <View style={styles.block}>
+      <Text>주식 통장</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  block: {},
+});
+
+export default StockPassbookScreen;

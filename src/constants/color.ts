fbf9c50bcfe0ee84f54f7f86b2aeb09eca5cb2f8
manyyeon <udayeon@naver.com>
2022-09-07 12:49:56 +0900

    chore : color 추가

---
+++ src/constants/color.ts
@@ -0,0 +1,24 @@
+const color = {
+  /* kb 색상 */
+  kb: '#FFBB00',
+  gray: '#BEBEBE',
+  light_gray: '#E2E2E2',
+  light_gray2: '#ECECEC',
+  light_gray3: '#F3F3F3',
+  light_gray4: 'F9F9F9',
+  deep_green: '#245630',
+  deep: '#3A3A3A',
+  warm_gray_deep: '#544F4C',
+  light_warm_gray: '#D6D3D2',
+  warm_gray1: '#747271',
+  system_information: '#A4A1A0',
+  system_warning: '#FF5242',
+  system_ok: '#3B9A50',
+  white: '#FFFFFF',
+  black: '#000000',
+  inactive: '#E3E3E3',
+  blue: '#428EFF',
+  light_yellow: '#FFEEBF',
+};
+
+export default color;

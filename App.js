6f3d599e8fe745865f3e5797b19e9d98e7295095
manyyeon <udayeon@naver.com>
2022-09-09 19:30:00 +0900

    chore : ios splash 추가

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import RootStack from './src/screens/RootStack';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    try {
+      setTimeout(() => {
+        SplashScreen.hide(); /** 추가 **/
+      }, 1000); /** 스플래시 시간 조절 (2초) **/
+    } catch (e) {
+      console.warn('에러발생');
+      console.warn(e);
+    }
+  }, []);
   return (
     <NavigationContainer>
       <RootStack />

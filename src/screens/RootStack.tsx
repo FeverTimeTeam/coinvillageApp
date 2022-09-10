9161e1a274b6a67f5f58b46586e484d52fecd581
manyyeon <udayeon@naver.com>
2022-09-11 05:54:14 +0900

    feat : 첫 화면 설정해주는 것 RootStack에서 설정

--- src/screens/RootStack.tsx
+++ src/screens/RootStack.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './HomeScreen';
 import JobScreen from './JobScreen';
@@ -12,71 +12,152 @@ import InstallmentSettingScreen from './passbook/InstallmentSettingScreen';
 import LoginScreen from './LoginScreen';
 import LogoutScreen from './LogoutScreen';
 import SignUpScreen from './SignUpScreen';
+import AboutFirstScreen from './about/AboutFirstScreen';
+import AboutSecondScreen from './about/AboutSecondScreen';
+import AboutThirdScreen from './about/AboutThirdScreen';
+import AboutFourthScreen from './about/AboutFourthScreen';
+import AboutFifthScreen from './about/AboutFifthScreen';
+import AboutLastScreen from './about/AboutLastScreen';
+import useAuthLoadEffect from '../hooks/useAuthLoadEffect';
+import {useRecoilValue, useRecoilState} from 'recoil';
+import {firstState} from '../atoms/first';
+import {useEffect} from 'react';
 
 const Stack = createNativeStackNavigator();
 
 const RootStack = () => {
+  async function useFirstState() {
+    await useAuthLoadEffect();
+    setIsLoading(false);
+  }
+
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [firstScreenState, setFirstScreenState] = useRecoilState(firstState);
+
+  useFirstState();
+
+  useEffect(() => {
+    console.log(isLoading);
+  }, [isLoading]);
+
+  console.log(firstScreenState.name);
   return (
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen
-        name="Login"
-        component={LoginScreen}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="Logout"
-        component={LogoutScreen}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="SignUp"
-        component={SignUpScreen}
-        // options={{
-        //   headerShown: false,
-        // }}
-      />
-      <Stack.Screen
-        name="Job"
-        component={JobScreen}
-        options={{title: '직업'}}
-      />
-      <Stack.Screen name="Investment" component={InvestmentScreen} />
-      <Stack.Screen
-        name="Passbook"
-        component={PassbookScreen}
-        options={{title: '통장'}}
-      />
-      <Stack.Screen
-        name="BasePassbook"
-        component={BasePassbookScreen}
-        options={{title: '입출금 통장'}}
-      />
-      <Stack.Screen
-        name="InstallmentPassbook"
-        component={InstallmentPassbookScreen}
-        options={{title: '적금 통장'}}
-      />
-      <Stack.Screen
-        name="StockPassbook"
-        component={StockPassbookScreen}
-        options={{title: '주식 통장'}}
-      />
-      <Stack.Screen
-        name="Consume"
-        component={ConsumeScreen}
-        options={{title: '입출금 통장'}}
-      />
-      <Stack.Screen
-        name="InstallmentSetting"
-        component={InstallmentSettingScreen}
-        options={{title: '적금 통장'}}
-      />
-    </Stack.Navigator>
+    isLoading === false && (
+      <Stack.Navigator initialRouteName={firstScreenState.name}>
+        <Stack.Screen
+          name="AboutFirst"
+          component={AboutFirstScreen}
+          options={{
+            headerShown: false,
+            animation: 'fade',
+          }}
+        />
+        <Stack.Screen
+          name="AboutSecond"
+          component={AboutSecondScreen}
+          options={{
+            headerShown: false,
+            animation: 'fade',
+          }}
+        />
+        <Stack.Screen
+          name="AboutThird"
+          component={AboutThirdScreen}
+          options={{
+            headerShown: false,
+            animation: 'fade',
+          }}
+        />
+        <Stack.Screen
+          name="AboutFourth"
+          component={AboutFourthScreen}
+          options={{
+            headerShown: false,
+            animation: 'fade',
+          }}
+        />
+        <Stack.Screen
+          name="AboutFifth"
+          component={AboutFifthScreen}
+          options={{
+            headerShown: false,
+            animation: 'fade',
+          }}
+        />
+        <Stack.Screen
+          name="AboutLast"
+          component={AboutLastScreen}
+          options={{
+            headerShown: false,
+            animation: 'fade',
+          }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Logout"
+          component={LogoutScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUpScreen}
+          // options={{
+          //   headerShown: false,
+          // }}
+        />
+        <Stack.Screen
+          name="Job"
+          component={JobScreen}
+          options={{title: '직업'}}
+        />
+        <Stack.Screen name="Investment" component={InvestmentScreen} />
+        <Stack.Screen
+          name="Passbook"
+          component={PassbookScreen}
+          options={{title: '통장'}}
+        />
+        <Stack.Screen
+          name="BasePassbook"
+          component={BasePassbookScreen}
+          options={{title: '입출금 통장'}}
+        />
+        <Stack.Screen
+          name="InstallmentPassbook"
+          component={InstallmentPassbookScreen}
+          options={{title: '적금 통장'}}
+        />
+        <Stack.Screen
+          name="StockPassbook"
+          component={StockPassbookScreen}
+          options={{title: '주식 통장'}}
+        />
+        <Stack.Screen
+          name="Consume"
+          component={ConsumeScreen}
+          options={{title: '입출금 통장'}}
+        />
+        <Stack.Screen
+          name="InstallmentSetting"
+          component={InstallmentSettingScreen}
+          options={{title: '적금 통장'}}
+        />
+      </Stack.Navigator>
+    )
   );
 };
 

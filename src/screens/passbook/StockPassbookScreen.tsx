77301e44b1c2e9b0b8e17d4fe24472c9086e0ec4
manyyeon <udayeon@naver.com>
2022-09-12 05:17:34 +0900

    feat : 주식 통장 종목 리스트 그림자 넣기

--- src/screens/passbook/StockPassbookScreen.tsx
+++ src/screens/passbook/StockPassbookScreen.tsx
@@ -28,34 +28,34 @@ const StockPassbookScreen = () => {
     },
     {
       stockId: 1,
-      content: '선생님 몸무게',
-      amount: 2,
+      content: '학급 총 지각 횟수',
+      amount: 34,
     },
     {
       stockId: 2,
-      content: '선생님 몸무게',
-      amount: 2,
+      content: '선생님 손소독 횟수',
+      amount: 234,
     },
     {
       stockId: 3,
       content: '선생님 몸무게',
-      amount: 2,
+      amount: 1,
     },
     {
       stockId: 4,
       content: '선생님 몸무게',
-      amount: 2,
+      amount: 45,
     },
     {
       stockId: 5,
       content: '선생님 몸무게',
-      amount: 2,
+      amount: 5,
     },
   ]);
 
   return (
     <View style={styles.block}>
-      <View style={styles.paddingHorizontal}>
+      <View style={styles.marginHorizontal}>
         <View style={styles.balanceWrapper}>
           <View style={[styles.backgroundGreen, styles.balance]}>
             <Text style={[styles.textBig]}>
@@ -63,42 +63,57 @@ const StockPassbookScreen = () => {
             </Text>
           </View>
         </View>
-        <View>
+        <View style={[styles.descriptionText]}>
           <Text style={[styles.textColorSystem]}>
             *종목 구매는 ‘투자' 탭에서 이루어집니다.
           </Text>
         </View>
-        <TouchableOpacity
-          style={[styles.submitButton, styles.backgroundGreen]}
-          onPress={() => {}}>
-          <Text style={[styles.bold, styles.textHuge, styles.textColorGreen]}>
-            입금하기
-          </Text>
-        </TouchableOpacity>
+        <View style={styles.passbookButtonWrapper}>
+          <PassbookButton
+            buttonText="입금하기"
+            textColor={color.green}
+            backgroundColor={color.light_green2}
+            borderColor={color.light_green2}
+            onPress={() => {
+              navigation.navigate('StockPassbookDeposit');
+            }}
+          />
+        </View>
+        <View style={styles.passbookButtonWrapper}>
+          <PassbookButton
+            buttonText="거래내역"
+            textColor={color.green}
+            backgroundColor={color.white}
+            borderColor={color.light_green3}
+            onPress={() => {
+              navigation.navigate('StockTransaction');
+            }}
+          />
+        </View>
       </View>
       <View style={styles.separatorBar} />
-      <View style={styles.paddingHorizontal}>
+      <View style={styles.marginHorizontal}>
         <View style={styles.stock}>
           <Text style={[styles.textHuge, styles.bold]}>보유 종목</Text>
         </View>
-
         <FlatList
-          style={styles.detailContentList}
+          style={[styles.detailContentList]}
+          ListFooterComponent={<View style={styles.footer} />}
           data={stockPassbookList}
           renderItem={({item}) => (
-            <View style={styles.detailContentContainer} key={item.stockId}>
-              <View style={styles.dateContainer}>
-                <Text style={styles.date}>{item.createdAt}</Text>
-              </View>
-              <View style={styles.contentContainer}>
-                <Text style={[styles.contentName, styles.textMid]}>
-                  {item.content}
-                </Text>
-                <Text style={[styles.amount, styles.bold, styles.textBig]}>
-                  {item.amount}주
-                </Text>
-              </View>
-            </View>
+            <TouchableOpacity
+              style={styles.contentContainer}
+              key={item.stockId}
+              onPress={() => {
+                navigation.navigate('StockDetail', {
+                  stockId: item.stockId,
+                  content: item.content,
+                  amount: item.amount,
+                });
+              }}>
+              <Text style={[styles.textMid]}>{item.content}</Text>
+              <Text style={[styles.bold, styles.textBig]}>{item.amount}주</Text>
+            </TouchableOpacity>
           )}
         />
       </View>
@@ -109,11 +124,13 @@ const StockPassbookScreen = () => {
 const styles = StyleSheet.create({
   block: {
     flex: 1,
-    // justifyContent: 'space-between',
     backgroundColor: `${color.white}`,
   },
-  paddingHorizontal: {
-    paddingHorizontal: 16,
+  footer: {
+    height: 350,
+  },
+  marginHorizontal: {
+    marginHorizontal: 16,
   },
   textSmall: {
     fontSize: 12,
@@ -151,6 +168,18 @@ const styles = StyleSheet.create({
     alignItems: 'flex-end',
     marginTop: 15,
     marginBottom: 32,
+    ...Platform.select({
+      ios: {
+        shadowColor: `${color.warm_gray_deep}`, //그림자색
+        shadowOpacity: 0.1, //그림자 투명도
+        shadowOffset: {width: 1, height: 1}, //그림자 위치
+        shadowRadius: 3,
+      },
+      android: {
+        //ANDROID
+        elevation: 3,
+      },
+    }),
   },
   balance: {
     width: 117,
@@ -159,6 +188,9 @@ const styles = StyleSheet.create({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  descriptionText: {
+    marginBottom: 10,
   },
   stock: {
     marginTop: 15,
@@ -173,19 +205,36 @@ const styles = StyleSheet.create({
     marginTop: 13,
     marginBottom: 15,
   },
+  passbookButtonWrapper: {
+    marginBottom: 11,
+    ...Platform.select({
+      ios: {
+        shadowColor: `${color.warm_gray_deep}`, //그림자색
+        shadowOpacity: 0.2, //그림자 투명도
+        shadowOffset: {width: 2, height: 2}, //그림자 위치
+        shadowRadius: 3,
+      },
+      android: {
+        //ANDROID
+        elevation: 3,
+      },
+    }),
+  },
   detailContentList: {},
   contentContainer: {
     backgroundColor: `${color.white}`,
     height: 71,
+    width: '99%',
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingHorizontal: 30,
     borderRadius: 16,
+    marginBottom: 16,
     ...Platform.select({
       ios: {
-        shadowColor: '#111', //그림자색
+        shadowColor: `${color.warm_gray_deep}`, //그림자색
         shadowOpacity: 0.2, //그림자 투명도
         shadowOffset: {width: 2, height: 3}, //그림자 위치
         shadowRadius: 3,

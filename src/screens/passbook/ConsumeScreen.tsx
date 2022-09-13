edc7fbc2000e6a5c480b8a2e4be6ec941b0de458
manyyeon <udayeon@naver.com>
2022-09-13 19:35:02 +0900

    feat : 소비하기 페이지 키보드가 버튼 안 가리게 바꿈

--- src/screens/passbook/ConsumeScreen.tsx
+++ src/screens/passbook/ConsumeScreen.tsx
@@ -1,6 +1,8 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   Alert,
+  KeyboardAvoidingView,
+  NativeModules,
   Platform,
   Pressable,
   StyleSheet,
@@ -10,11 +12,10 @@ import {
 } from 'react-native';
 import PassbookButton from '../../components/PassbookButton';
 import color from '../../constants/color';
-import {useEffect} from 'react';
 import {axiosInstance} from '../../queries/index';
 import {useNavigation} from '@react-navigation/native';
 import {useRecoilState} from 'recoil';
-import {basePassbookState} from '../../atoms/basePassbook';
+import {basePassbookListState} from '../../atoms/basePassbook';
 
 const ConsumeScreen = () => {
   const navigation = useNavigation();
@@ -30,129 +31,157 @@ const ConsumeScreen = () => {
     price: 0,
     total: 0,
   });
-  const [balance, setBalance] = useState<number>(537);
   const {content, count, price, total} = item;
 
-  const [basePassbookList, setBasePassbookList] =
-    useRecoilState(basePassbookState);
+  const [basePassbookList, setBasePassbookList] = useRecoilState(
+    basePassbookListState,
+  );
 
-  const getBasePassbookList = () => {
+  const buyContent = () => {
     axiosInstance
-      .get('/accounts')
+      .post('/accounts', {
+        content: content,
+        count: count,
+        total: count * price,
+      })
       .then(response => {
         console.log(response.data);
-        setBasePassbookList({items: response.data.reverse()});
       })
       .catch(e => {
         console.log(e);
       });
   };
 
+  const {StatusBarManager} = NativeModules;
+  const [statusBarHeight, setStatusBarHeight] = useState(0);
+  useEffect(() => {
+    if (Platform.OS === 'ios') {
+      StatusBarManager.getHeight(statusBarFrameData => {
+        setStatusBarHeight(statusBarFrameData.height);
+      });
+    }
+  }, [StatusBarManager]);
   return (
-    <View style={styles.block}>
-      <View style={styles.balanceContainer}>
-        <View style={styles.balanceView}>
-          <Text style={styles.balanceText}>
-            잔액{' '}
-            <Text style={styles.bold}>
-              {basePassbookList.items[0].accountTotal}
-            </Text>{' '}
-            리브
-          </Text>
+    <KeyboardAvoidingView
+      style={styles.block}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={statusBarHeight + 44}>
+      <View>
+        <View style={styles.balanceContainer}>
+          <View style={styles.balanceView}>
+            <Text style={styles.balanceText}>
+              잔액{' '}
+              <Text style={styles.bold}>
+                {basePassbookList.items[0].accountTotal}
+              </Text>{' '}
+              리브
+            </Text>
+          </View>
         </View>
-      </View>
-      <View style={styles.itemNameContainer}>
-        <Text style={styles.itemNameText}>품목</Text>
-        <TextInput
-          style={[styles.input, styles.itemNameInput]}
-          value={item.content}
-          onChange={e => {
-            setItem({
-              ...item,
-              content: e.nativeEvent.text,
-            });
-          }}
-        />
-      </View>
-      <View style={styles.priceNumContainer}>
-        <View style={styles.priceContainer}>
-          <Text style={styles.priceText}>1개 당 가격</Text>
+        <View style={styles.itemNameContainer}>
+          <Text style={styles.itemNameText}>품목</Text>
           <TextInput
-            style={[styles.input, styles.priceInput]}
-            value={item.price.toString()}
+            style={[styles.input, styles.itemNameInput]}
+            value={item.content}
             onChange={e => {
-              const tmpPrice = parseInt(e.nativeEvent.text);
-              if (!isNaN(tmpPrice)) {
-                setItem({
-                  ...item,
-                  price: tmpPrice,
-                  total: tmpPrice * count,
-                });
-              } else if (e.nativeEvent.text === '') {
-                setItem({
-                  ...item,
-                  price: 0,
-                });
-              } else {
-                console.log('숫자만 입력');
-              }
+              setItem({
+                ...item,
+                content: e.nativeEvent.text,
+              });
             }}
           />
         </View>
-        <View style={styles.multiplyWrapper}>
-          <Text style={styles.multiply}>X</Text>
-        </View>
-        <View style={styles.numContainer}>
-          <Text style={styles.numText}>개수</Text>
-          <TextInput
-            style={[styles.input, styles.numInput]}
-            value={item.count.toString()}
-            onChange={e => {
-              const tmpCount = parseInt(e.nativeEvent.text);
-              if (!isNaN(tmpCount)) {
-                setItem({
-                  ...item,
-                  count: tmpCount,
-                  total: tmpCount * price,
-                });
-              } else if (e.nativeEvent.text === '') {
-                setItem({
-                  ...item,
-                  count: 0,
-                });
-              } else {
-                console.log('숫자만 입력');
-              }
-            }}
-          />
+        <View style={styles.priceNumContainer}>
+          <View style={styles.priceContainer}>
+            <Text style={styles.priceText}>1개 당 가격</Text>
+            <TextInput
+              style={[styles.input, styles.priceInput]}
+              value={item.price.toString()}
+              onChange={e => {
+                const tmpPrice = parseInt(e.nativeEvent.text);
+                if (!isNaN(tmpPrice)) {
+                  setItem({
+                    ...item,
+                    price: tmpPrice,
+                    total: tmpPrice * count,
+                  });
+                } else if (e.nativeEvent.text === '') {
+                  setItem({
+                    ...item,
+                    price: 0,
+                  });
+                } else {
+                  console.log('숫자만 입력');
+                }
+              }}
+            />
+          </View>
+          <View style={styles.multiplyWrapper}>
+            <Text style={styles.multiply}>X</Text>
+          </View>
+          <View style={styles.numContainer}>
+            <Text style={styles.numText}>개수</Text>
+            <TextInput
+              style={[styles.input, styles.numInput]}
+              value={item.count.toString()}
+              onChange={e => {
+                const tmpCount = parseInt(e.nativeEvent.text);
+                if (!isNaN(tmpCount)) {
+                  setItem({
+                    ...item,
+                    count: tmpCount,
+                    total: tmpCount * price,
+                  });
+                } else if (e.nativeEvent.text === '') {
+                  setItem({
+                    ...item,
+                    count: 0,
+                  });
+                } else {
+                  console.log('숫자만 입력');
+                }
+              }}
+            />
+          </View>
         </View>
       </View>
-      <View style={styles.separatorBar} />
       <View style={styles.consumeButtonContainer}>
+        <View style={styles.separatorBar} />
         <Text style={styles.totalPrice}>
           총 <Text style={styles.bold}>{item?.total}</Text>미소 차감
         </Text>
         <PassbookButton
+          style={styles.buyButton}
           buttonText="구매하기"
-          onPress={async () => {
-            await axiosInstance
-              .post('/accounts', {
-                content: content,
-                count: count,
-                total: count * price,
-              })
-              .then(response => {
-                console.log(response.data);
-              })
-              .catch(e => {
-                console.log(e);
-              });
-
-            await getBasePassbookList();
+          onPress={() => {
+            Alert.alert(
+              `${content}를 ${count}개 구매하시겠습니까?`,
+              `총 ${total} 미소 차감`,
+              [
+                {
+                  text: '구매하기',
+                  onPress: () => {
+                    buyContent();
+                    navigation.pop();
+                    Alert.alert('구매 완료!', `총 ${total} 리브 차감`, [
+                      {
+                        text: '확인',
+                        onPress: () => {},
+                      },
+                    ]);
+                  },
+                },
+                {
+                  text: '취소하기',
+                  onPress: () => {},
+                  style: 'cancel',
+                },
+              ],
+            );
           }}
         />
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -161,6 +190,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: `${color.white}`,
     paddingHorizontal: 16,
+    display: 'flex',
+    justifyContent: 'space-between',
   },
   balanceContainer: {
     height: 35,
@@ -200,7 +231,7 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'center',
-    height: 380,
+    // height: 380,
     paddingHorizontal: 9,
   },
   priceContainer: {
@@ -235,11 +266,8 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   consumeButtonContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 16,
-    right: 16,
-    height: 192,
+    marginTop: 20,
+    marginBottom: 30,
     width: '100%',
     display: 'flex',
     justifyContent: 'center',
@@ -261,8 +289,9 @@ const styles = StyleSheet.create({
   separatorBar: {
     height: 7,
     backgroundColor: `${color.light_gray2}`,
-    marginTop: 30,
+    // marginTop: 30,
   },
+  buyButton: {},
 });
 
 export default ConsumeScreen;
